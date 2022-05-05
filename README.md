1c9a5bfb9dc90fd26a4bab5247e26bc83988a965
薛猷騰 <uton@uton.local>
2022-05-05 11:27:57 +0800

    Create canvas, draw pixels

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 🚀 Welcome to your new awesome project!
+# 🚀 Ray Tracer js!
 
 This project has been created using **webpack-cli**, you can now run
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+import { drawPixels } from "./drawPixels";
+
+const canvas = document.getElementById("canvas");
+const width = 600,
+  height = 600;
+const arr = new Uint8ClampedArray(4 * width * height);
+
+// Iterate through every pixel
+for (let i = 0; i < arr.length; i += 4) {
+  arr[i + 0] = 100; // R value
+  arr[i + 1] = 190; // G value
+  arr[i + 2] = 0; // B value
+  arr[i + 3] = 255; // A value
+}
+
+drawPixels(canvas, arr, width, height);

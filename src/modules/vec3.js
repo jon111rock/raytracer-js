497376ea029d5d95756381a79301d462c5455e7d
薛猷騰 <uton@uton.local>
2022-05-05 22:43:33 +0800

    Add dot, cross function

--- src/modules/vec3.js
+++ src/modules/vec3.js
@@ -32,3 +32,15 @@ export class Vector3 {
     return Math.sqrt(this.lengthSquared());
   }
 }
+
+export function dot(v1, v2) {
+  return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+export function cross(v1, v2) {
+  return new Vector3(
+    v1.y * v2.z - v1.z * v2.y,
+    v1.z * v2.x - v1.x * v2.z,
+    v1.x * v2.y - v1.y * v2.x
+  );
+}

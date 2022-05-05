4633310ac6e444f001b2eb741205c8902d579cb3
薛猷騰 <uton@uton.local>
2022-05-05 11:53:36 +0800

    Make a color image

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import { drawPixels } from "./drawPixels";
 
 const canvas = document.getElementById("canvas");
-const width = 600,
-  height = 600;
-const arr = new Uint8ClampedArray(4 * width * height);
+const width = 256,
+  height = 256;
+const pixels = new Uint8ClampedArray(4 * width * height);
 
 // Iterate through every pixel
-for (let i = 0; i < arr.length; i += 4) {
-  arr[i + 0] = 100; // R value
-  arr[i + 1] = 190; // G value
-  arr[i + 2] = 0; // B value
-  arr[i + 3] = 255; // A value
+for (let j = height - 1; j >= 0; j--) {
+  for (let i = 0; i < width; i++) {
+    let r = i / (width - 1);
+    let g = j / (height - 1);
+    let b = 0.25;
+
+    let ir = parseInt(255.99 * r);
+    let ig = parseInt(255.99 * g);
+    let ib = parseInt(255.99 * b);
+    const index = ((height - 1 - j) * width + i) * 4;
+    pixels[index + 0] = ir; // R value
+    pixels[index + 1] = ig; // G value
+    pixels[index + 2] = ib; // B value
+    pixels[index + 3] = 255; // A value
+  }
 }
 
-drawPixels(canvas, arr, width, height);
+drawPixels(canvas, pixels, width, height);

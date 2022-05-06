6c4c0803955ff2c88a68c3799a2c69fdd85729b6
薛猷騰 <uton@uton.local>
2022-05-06 13:12:15 +0800

    Add type in light class

--- src/modules/light.js
+++ src/modules/light.js
@@ -2,8 +2,10 @@ export class Light {
   /**
    *
    * @param {float} intensity float
+   * @constant  type "ambient" (string)
    */
   static ambient(intensity) {
+    this.type = "ambient";
     this.intensity = intensity;
   }
 
@@ -11,8 +13,10 @@ export class Light {
    *
    * @param {float} intensity float
    * @param {vector3} position vector3
+   * @constant  type "point" (string)
    */
   static point(intensity, position) {
+    this.type = "point";
     this.intensity = intensity;
     this.position = position;
   }
@@ -21,8 +25,10 @@ export class Light {
    *
    * @param {float} intensity float
    * @param {vector3} position vector3
+   * @constant  type "directional" (string)
    */
   static directional(intensity, position) {
+    this.type = "directional";
     this.intensity = intensity;
     this.position = position;
   }
